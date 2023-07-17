4dfd05e20993e384dc1482b8c903fd134d8cd0e0
Vitaly Gaidukov <vitaly.gaidukov@gmail.com>
2023-07-17 19:56:50 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,21 +1,22 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Entity//помечает классы связанные с базой данных
+@Table(name = "user")
 public class User {
-    @Id
+    @Id//для первичных ключей указывается аннотация
+    @Column(name = "id")//сопоставляет каждое из полей со столбцами
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "lastName")
     private String lastName;
 
-    @Column
+    @Column(name = "age")
     private Byte age;
 
     public User() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,17 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
+import javax.imageio.spi.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb2";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "root";
     private static Connection connection;
-    private static SessionFactory sessionFactory;
 
 
     public static Connection getConnection() {
@@ -24,4 +30,26 @@ public class Util {
         }
         return connection;
     }
+
+//    public static SessionFactory getSessionFactory(){
+//        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+//        return configuration.buildSessionFactory();
+//
+//    }
+public static SessionFactory getSessionFactory(){
+        Configuration configuration = new Configuration();
+    Properties properties = new Properties();
+    properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+    properties.put(Environment.URL, URL);
+    properties.put(Environment.USER, USER_NAME);
+    properties.put(Environment.PASS, PASSWORD);
+    properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+    properties.put(Environment.SHOW_SQL, "true");
+    properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+    properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+    configuration.setProperties(properties);
+    SessionFactory sessionFactory = configuration.addAnnotatedClass(User.class).addProperties(configuration.getProperties()).buildSessionFactory();
+    return sessionFactory;
+
+}
 }
