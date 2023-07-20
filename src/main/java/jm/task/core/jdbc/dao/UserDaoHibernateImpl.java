08979967c0dacaeea73ca8a8c8bee88e6dfcda48
Vitaly Gaidukov <vitaly.gaidukov@gmail.com>
2023-07-20 17:14:00 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -16,8 +16,10 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.getCurrentSession()) {
+        SessionFactory sessionFactory = Util.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try (sessionFactory;
+             session) {
             session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS user" +
                     "(" +
@@ -28,48 +30,55 @@ public class UserDaoHibernateImpl implements UserDao {
                     ")").addEntity(User.class).executeUpdate();
             session.getTransaction().commit();
         }catch (IllegalStateException e){
-            System.out.println("При тестировании создания таблицы пользователей произошло исключение\n" + e.getMessage() + Arrays.toString(e.getStackTrace()));
+            session.getTransaction().rollback();
         }
     }
 
     @Override
     public void dropUsersTable() {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.getCurrentSession()) {
+        SessionFactory sessionFactory = Util.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try (sessionFactory;
+             session) {
             session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
             session.getTransaction().commit();
         }catch (IllegalStateException e){
-            System.out.println("При тестировании удаления таблицы произошло исключение\n" + e.getMessage() + Arrays.toString(e.getStackTrace()));
+            session.getTransaction().rollback();
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.getCurrentSession()) {
+        SessionFactory sessionFactory = Util.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try (sessionFactory;
+             session) {
             session.beginTransaction();
             session.save(new User(name, lastName, age));
             session.getTransaction().commit();
         }catch (IllegalStateException e){
-            System.out.println("Во время тестирования сохранения пользователя произошло исключение\n" + e.getMessage() + Arrays.toString(e.getStackTrace()));
+            session.getTransaction().rollback();
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.getCurrentSession()) {
+        SessionFactory sessionFactory = Util.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try (sessionFactory;
+             session) {
             session.beginTransaction();
             session.createSQLQuery("DELETE FROM user WHERE ID =id");
             session.getTransaction().commit();
         }catch (IllegalStateException e){
-            System.out.println("При тестировании удаления пользователя по id произошло исключение\n" + e.getMessage() + Arrays.toString(e.getStackTrace()));
+            session.getTransaction().rollback();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
+
         try (SessionFactory sessionFactory = Util.getSessionFactory();
              Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
@@ -79,13 +88,15 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try (SessionFactory sessionFactory = Util.getSessionFactory();
-             Session session = sessionFactory.getCurrentSession()) {
+        SessionFactory sessionFactory = Util.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try (sessionFactory;
+             session) {
             session.beginTransaction();
             session.createQuery("DELETE FROM User").executeUpdate();
             session.getTransaction().commit();
         }catch (IllegalStateException e){
-            System.out.println("При тестировании очистки таблицы пользователей произошло исключение\n" + e.getMessage() + Arrays.toString(e.getStackTrace()));
+            session.getTransaction().rollback();
         }
     }
 }
